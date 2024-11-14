2734958412f56ef880857fc1273afac8322868cc
andradew <wilson00749@gmail.com>
2024-11-13 23:50:31 -0500

    Use Lombok

--- src/main/java/com/inventario/inventario_api/model/User.java
+++ src/main/java/com/inventario/inventario_api/model/User.java
@@ -1,9 +1,15 @@
 package com.inventario.inventario_api.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "usuarios") //TODO al usar user da conflito con H2 y Postgres
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -14,14 +20,9 @@ public class User {
     private String email;
     private String password;
 
-    // Default constructor for JPA
-    public User() {}
-
-    // Constructor for easy creation
     public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
     }
-
 }

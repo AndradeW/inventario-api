8f9601138106d28cfaed120bec6d6686d1b37e5c
AndradeW <75168527+AndradeW@users.noreply.github.com>
2024-11-16 14:48:11 -0500

    Add password Encryption and UserMapper (#2)
    * Add password Encryption, remove mapstruct and create UserMapper
    
    * Use UserDTO in UpdateUser method

--- src/main/java/com/inventario/inventario_api/DTO/Mapper/UserMapper.java
+++ src/main/java/com/inventario/inventario_api/DTO/Mapper/UserMapper.java
@@ -3,15 +3,71 @@ package com.inventario.inventario_api.DTO.Mapper;
 import com.inventario.inventario_api.DTO.UserDTO;
 import com.inventario.inventario_api.DTO.UserInputDTO;
 import com.inventario.inventario_api.model.User;
-import org.mapstruct.Mapper;
+import com.inventario.inventario_api.security.BCryptEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface UserMapper {
+@Component
+public class UserMapper {
 
-    UserDTO userToUserDTO(User user);
+    private final BCryptEncoder encoder;
 
-    User userDTOToUser(UserDTO userDTO);
+    @Autowired
+    public UserMapper(BCryptEncoder encoder) {
+        this.encoder = encoder;
+    }
 
-    User userInputToUserInputDTO(UserInputDTO userInputDTO);
+    public UserDTO userToUserDTO(User user) {
+        if (user == null) {
+            return null;
+        }
 
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setId(user.getId());
+        userDTO.setName(user.getName());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setRole(user.getRole());
+        userDTO.setAddress(user.getAddress());
+        userDTO.setPhone(user.getPhone());
+
+        return userDTO;
+    }
+
+    public User userDTOToUser(UserDTO userDTO) {
+        if (userDTO == null) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId(userDTO.getId());
+        user.setName(userDTO.getName());
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setRole(userDTO.getRole());
+        user.setAddress(userDTO.getAddress());
+        user.setPhone(userDTO.getPhone());
+
+        return user;
+    }
+
+    public User userInputToUserInputDTO(UserInputDTO userInputDTO) {
+        if (userInputDTO == null) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setName(userInputDTO.getName());
+        user.setUsername(userInputDTO.getUsername());
+        user.setEmail(userInputDTO.getEmail());
+        user.setPassword(encoder.encodePassword(userInputDTO.getPassword()));
+        user.setRole(userInputDTO.getRole());
+        user.setAddress(userInputDTO.getAddress());
+        user.setPhone(userInputDTO.getPhone());
+
+        return user;
+    }
 }

a4b3398d4e281c60458c5f70822af80038aa9eb2
andradew <wilson00749@gmail.com>
2024-11-16 20:45:03 -0500

    Fix requestMatchers

--- src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
+++ src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.inventario.inventario_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,13 +16,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .authorizeHttpRequests(customRequest -> {
+                    customRequest
+                            .requestMatchers("/auth/**").permitAll()
+                            .anyRequest().authenticated();
+                })
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        (customizeRequests) -> customizeRequests
-                                //.requestMatchers("/auth/**").permitAll()
-                                .anyRequest().authenticated()
-                        //.anyRequest().authenticated()
-                );
+                .cors(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }

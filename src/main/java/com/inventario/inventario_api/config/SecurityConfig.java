88bb318254d866674d6a101eb5a172a611df72ee
andradew <wilson00749@gmail.com>
2024-11-16 20:21:23 -0500

    Add Security and Cors config, pom imports

---
+++ src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.inventario.inventario_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                //.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        (customizeRequests) -> customizeRequests
+                                //.requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                        //.anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+}

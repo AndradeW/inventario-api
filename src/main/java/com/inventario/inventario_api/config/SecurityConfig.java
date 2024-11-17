181b104fc02ea65feba6d6b6fba42f83e03f9a6a
andradew <wilson00749@gmail.com>
2024-11-16 20:23:19 -0500

    Disable csrf

--- src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
+++ src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                //.csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         (customizeRequests) -> customizeRequests
                                 //.requestMatchers("/auth/**").permitAll()

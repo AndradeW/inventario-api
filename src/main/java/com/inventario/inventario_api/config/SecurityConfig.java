9950066a9a61ee7b3b920965305e27109d6bedb1
andradew <wilson00749@gmail.com>
2024-11-17 23:56:03 -0500

    Add internalChainFilter and Remove sessions

--- src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
+++ src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
@@ -6,26 +6,38 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeHttpRequests(customRequest -> {
-                    customRequest
-                            .requestMatchers("/api/auth/**").permitAll()
-                            .anyRequest().authenticated();
-                })
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                .sessionManagement(
+                        sessions ->
+                                sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(
+                        customRequest -> customRequest
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated())
+
+                .addFilterBefore(this.jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }

73e5e64d7b39c28abbde930ab9c143ce3ad4da76
andradew <wilson00749@gmail.com>
2024-11-17 22:51:34 -0500

    Add isValid and getUsername JwtUtil methods

--- src/main/java/com/inventario/inventario_api/security/JwtUtil.java
+++ src/main/java/com/inventario/inventario_api/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.inventario.inventario_api.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -23,6 +24,24 @@ public class JwtUtil {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1))) // 1 hora
                 .sign(ALGORITHM);
+    }
+
+    public boolean isValid(String token) {
+        try {
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(token);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String token) {
+        return JWT.require(ALGORITHM)
+                .build()
+                .verify(token)
+                .getSubject();
     }
 
     @Bean

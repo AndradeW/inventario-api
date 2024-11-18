295a07a7e3e5aa7636d65e55d1e9069b27f2d5cc
andradew <wilson00749@gmail.com>
2024-11-17 22:41:53 -0500

    Implement UserDetailsService to create users in memory (Or DB)

--- src/main/java/com/inventario/inventario_api/security/JwtUtil.java
+++ src/main/java/com/inventario/inventario_api/security/JwtUtil.java
@@ -2,6 +2,9 @@ package com.inventario.inventario_api.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -20,6 +23,11 @@ public class JwtUtil {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1))) // 1 hora
                 .sign(ALGORITHM);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
--- src/main/java/com/inventario/inventario_api/service/UserService.java
+++ src/main/java/com/inventario/inventario_api/service/UserService.java
@@ -3,13 +3,17 @@ package com.inventario.inventario_api.service;
 import com.inventario.inventario_api.model.UserEntity;
 import com.inventario.inventario_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Autowired
@@ -36,4 +40,28 @@ public class UserService {
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return User.builder()
+                .username("johndoe")
+                .password("$2y$10$fnoMh/rIiBIHTZUmmXG2qOBKajG81uM/NR6i4A/vrDd83zhurCK7a")
+                .roles("ADMIN")
+                .accountLocked(false)
+                .disabled(false)
+                .build();
+    }
+
+
+//        User userEntity = this.userRepository.findById(Long.valueOf(username))
+//                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found."));
+//
+//        return User.builder()
+//                .username(userEntity.getUsername())
+//                .password(userEntity.getPassword())
+//                .role("ADMIN")
+//                .accountLocked(userEntity.isLocked())
+//                .disabled(userEntity.isDisabled())
+//                .build();
+//    }
 }

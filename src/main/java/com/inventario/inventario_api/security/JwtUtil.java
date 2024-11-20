f3321c40ca1b2c8f513c67f1733397b4e28d098e
andradew <wilson00749@gmail.com>
2024-11-19 21:55:46 -0500

    Use JWT environments variables

--- src/main/java/com/inventario/inventario_api/security/JwtUtil.java
+++ src/main/java/com/inventario/inventario_api/security/JwtUtil.java
@@ -3,6 +3,7 @@ package com.inventario.inventario_api.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -14,21 +15,26 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "secretKey";
-    private static final Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+    private final String issuer;
+    private final Algorithm ALGORITHM;
+
+    public JwtUtil(@Value("${app.jwt.issuer}") String issuer, @Value("${app.jwt.secret_key}") String secretKey) {
+        this.issuer = issuer;
+        this.ALGORITHM = Algorithm.HMAC256(secretKey);
+    }
 
     public String generateToken(String username) {
         return JWT.create()
                 .withSubject(username)
-                .withIssuer("invantario-api")
+                .withIssuer(this.issuer)
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1))) // 1 hora
-                .sign(ALGORITHM);
+                .sign(this.ALGORITHM);
     }
 
     public boolean isValid(String token) {
         try {
-            JWT.require(ALGORITHM)
+            JWT.require(this.ALGORITHM)
                     .build()
                     .verify(token);
             return true;
@@ -38,7 +44,7 @@ public class JwtUtil {
     }
 
     public String getUsername(String token) {
-        return JWT.require(ALGORITHM)
+        return JWT.require(this.ALGORITHM)
                 .build()
                 .verify(token)
                 .getSubject();

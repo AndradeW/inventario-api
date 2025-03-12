9e47d4f46a299aecc032142ec570f40ef1404a58
andradew <wilson00749@gmail.com>
2025-03-12 15:20:04 -0500

    Use WebMvcConfigurer

--- src/main/java/com/inventario/inventario_api/config/CorsConfig.java
+++ src/main/java/com/inventario/inventario_api/config/CorsConfig.java
@@ -2,27 +2,24 @@ package com.inventario.inventario_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS").toString());
-
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
+

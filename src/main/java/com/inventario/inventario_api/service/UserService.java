5d4fc6bc04b53cfe1d79a41d93863726be9b744b
andradew <wilson00749@gmail.com>
2025-03-13 19:55:30 -0500

    Override loadUserByUsername to load user from DB

--- src/main/java/com/inventario/inventario_api/service/UserService.java
+++ src/main/java/com/inventario/inventario_api/service/UserService.java
@@ -3,13 +3,18 @@ package com.inventario.inventario_api.service;
 import com.inventario.inventario_api.model.User;
 import com.inventario.inventario_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Autowired
@@ -19,21 +24,46 @@ public class UserService {
 
     // Create or update user
     public User saveUser(User user) {
-        return userRepository.save(user);
+        return this.userRepository.save(user);
     }
 
     // Get all users
     public List<User> getUsers() {
-        return userRepository.findAll();
+        return this.userRepository.findAll();
     }
 
     // Get user by ID
     public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
+        return this.userRepository.findById(id);
     }
 
     // Delete user by ID
     public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+        this.userRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = this.userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario " + username + " no existe."));
+
+        List<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
+
+        user.getRoles()
+                .forEach(role -> simpleGrantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+
+        user.getRoles().stream()
+                .flatMap(role -> role.getPermissionsList().stream())
+                .forEach(permission -> simpleGrantedAuthorities.add(new SimpleGrantedAuthority(permission.getName())));
+
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.isAccountNoExpired(),
+                user.isCredentialsNoExpired(),
+                user.isAccountNoLocked(),
+                simpleGrantedAuthorities);
     }
 }

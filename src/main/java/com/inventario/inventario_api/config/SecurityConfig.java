cf3f43cafc5ecc0b6a4003bf6ac118d82c76a96f
andradew <wilson00749@gmail.com>
2025-03-13 21:18:42 -0500

    Use BCrypt to encode passwords, add /auth/login to permitAll

--- src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
+++ src/main/java/com/inventario/inventario_api/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -30,9 +30,9 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(http -> {
-                            http.requestMatchers(HttpMethod.POST, "/auth/register").permitAll();
 
-                            http.requestMatchers(HttpMethod.GET, "/api/users").hasAnyAuthority("CREATE");
+                            http.requestMatchers(HttpMethod.POST, "/auth/register").permitAll();
+                            http.requestMatchers(HttpMethod.GET, "/auth/login").permitAll();
 
                             http.anyRequest().authenticated();
                         }
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(this.passwordEncoder());
+        provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(userDetailsService);
 
         return provider;
@@ -56,6 +56,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
